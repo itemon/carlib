d4df92c9bd828d2a708aca7ad26ea8e85971bddc
huangwei <itemon@gmail.com>
2018-12-01 11:10:14 +0800

    fix bug

--- __test__/data_brands.spec.js
+++ __test__/data_brands.spec.js
@@ -5,7 +5,7 @@ import { getBrands } from '../src/data/brands'
 test ('获取brands列表，测试集是否为空', async () => {
   expect.assertions(1)
   const brands = await getBrands()
-  console.log('all brands: ', brands)
+  console.log('all brands: ', brands.length)
   expect(brands.length).toBeGreaterThan(0)
 })
 
