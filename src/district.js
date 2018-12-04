abccde57e19fceb59b28f5188697216ba6dddb93
huangwei <itemon@gmail.com>
2018-12-04 18:20:10 +0800

    fix bug

--- src/district.js
+++ src/district.js
@@ -2,6 +2,7 @@
 
 import { createBrowser, destroyBrowser } from './browser/instance'
 import { toPinYinKey } from './utils/'
+import { join } from 'path'
 
 const getProvinces = async (page: any): Promise<Array<Province>> => {
   await page.goto('http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html')
@@ -63,11 +64,45 @@ const getCities = async (page: any, province: Province): Promise<Array<City>> =>
 
 let id = 100
 
+let abbrMap = {
+  '110000': '京',
+  '120000': '津',
+  '130000': '冀',
+  '140000': '晋',
+  '150000': '蒙',
+  '210000': '辽',
+  '220000': '吉',
+  '230000': '黑',
+  '310000': '沪',
+  '320000': '苏',
+  '330000': '浙',
+  '340000': '皖',
+  '350000': '闽',
+  '360000': '赣',
+  '370000': '鲁',
+  '410000': '豫',
+  '420000': '鄂',
+  '430000': '湘',
+  '440000': '粤',
+  '450000': '桂',
+  '460000': '琼',
+  '500000': '渝',
+  '510000': '川',
+  '520000': '贵',
+  '530000': '云',
+  '540000': '藏',
+  '610000': '陕',
+  '620000': '甘',
+  '630000': '青',
+  '640000': '宁',
+  '650000': '新',
+}
+
 const toSql = (p: Province, cities: Array<City>): string => {
   const parentId = ++id
   const sql = `
     insert into district (id, name, name_en, name_abbr, name_abbr_en, code, parent_id, create_time, update_time, type) values
-    (${ parentId }, '${ p.text }', '${ toPinYinKey(p.text) }', '', '', ${ p.code || '000000' }, 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'p')${ cities.length == 0 ? '' : `,` }
+    (${ parentId }, '${ p.text }', '${ toPinYinKey(p.text) }', '${ p.code ? abbrMap[p.code] : "" }', '', ${ p.code || '000000' }, 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'p')${ cities.length == 0 ? '' : `,` }
     ${
       cities.map(i => {
         let py = toPinYinKey(i.text)
@@ -78,18 +113,20 @@ const toSql = (p: Province, cities: Array<City>): string => {
   return sql
 }
 
-const updateDistrict = async () => {
+const updateDistrict = async (path: ?string) => {
   const bro = await createBrowser()
   const page = await bro.newPage()
 
   const provinces = await getProvinces(page)
   await page.close()
 
+  let sqls = []
+
   for (let p of provinces) {
     let newPage = await bro.newPage()
     let cities = await getCities(newPage, p)
     console.log(cities)
-    console.log(toSql(p, cities))
+    sqls.push(toSql(p, cities))
     await newPage.close()
   }
 
@@ -100,4 +137,5 @@ const updateDistrict = async () => {
 export {
   updateDistrict,
   getProvinces,
+  toSql,
 }
